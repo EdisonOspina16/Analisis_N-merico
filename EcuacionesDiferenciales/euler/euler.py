a342413d85944c838fe5fd2b9cbde25e67ffc8b9
Edison Ospina <140641793+EdisonOspina16@users.noreply.github.com>
2025-11-01 23:00:08 -0500

    euler

--- EcuacionesDiferenciales/euler/euler.py
+++ EcuacionesDiferenciales/euler/euler.py
@@ -2,13 +2,12 @@ import numpy as np
 
 def euler(funcion, a, b, h, condicion_inicial):
     """
-    Método de Euler para sistemas de EDOs.
-    :param funcion: función f(t, z) que devuelve un array con derivadas
-    :param a: límite inferior
-    :param b: límite superior
-    :param h: paso
-    :param condicion_inicial: vector de condiciones iniciales [x0, y0]
-    :return: arrays t y w (solución)
+    :parametro función: función f(t, z) que devuelve un array con derivadas
+    :parametro a: límite inferior
+    :parametro b: límite superior
+    :parametro h: paso
+    :parametro condición_inicial: vector de condiciones iniciales [x0, y0]
+    :retorna: arrays t y w (solución)
     """
 
     n = int((b - a) / h)
@@ -29,8 +28,8 @@ def funcion(t, z):
     f_2 = t * np.exp(t) - t + 2 * w - u
     return np.array([f_1, f_2])
 
-a = 0
-b = 1
-h = 0.5
-co = np.array([0, 0])
-print(euler(funcion, a, b, h, co))
+#a = 0
+#b = 1
+#h = 0.5
+#co = np.array([0, 0])
+#print(euler(funcion, a, b, h, co))
